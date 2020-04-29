642f36c8273830178470cf27a94f3aaff5b78132
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-04-28 17:07:56 -0700

    add 2 if statements

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
+'use strict';
 
+var lordOfTheRings = prompt ('Do I like lords of the rings?')
+
+if(lordOfTheRings.toLowerCase() === 'yes'){
+  alert('You\'re right, that\'s my favorite movie');
+} else if(lordOfTheRings.toLowerCase() === 'no'){
+  alert('Too bad, that\'s actually my favorite movie');
+}
+
+
+var brunette = prompt ('Am I a brunette?');
+
+if(brunette.toLowerCase() === 'yes'){
+  alert('You\'re right, I am a brunette');
+} else if(brunette.toLowerCase() === 'no'){
+  alert('Too bad, I am actually a brunette');
+}
